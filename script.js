ceb3bd51a8acd712cd8036d458b6f8ff67786f31
Jason John <jtj@amazon.com>
2024-10-13 17:27:50 -0500

    Website skeleton and introduction lesson

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-const md = window.markdownit();
-const contentDiv = document.getElementById('content');
-
-const pages = {
-  home: `# Welcome to the Markdown Website!\nThis is the **home page**.`,
-  toc: `
-# Table of Contents
-1. [Page 1](#page1)
-2. [Page 2](#page2)
-3. [Page 3](#page3)
-  `
-};
-
-async function loadPage(page) {
-  if (pages[page]) {
-    contentDiv.innerHTML = md.render(pages[page]);
-  } else {
-    try {
-      const response = await fetch(`markdown/${page}.md`);
-      const text = await response.text();
-      contentDiv.innerHTML = md.render(text);
-    } catch (error) {
-      contentDiv.innerHTML = "<p>Page not found.</p>";
-    }
-  }
-}
-
-// Load the home page initially
-loadPage('home');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
